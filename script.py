18f59b5a8c32b5f76e603090f6f5ed1712babd48
Tammam Faris <107762803+Tammam20@users.noreply.github.com>
2023-09-24 20:50:24 +0300

    Update script.py

--- script.py
+++ script.py
@@ -2,13 +2,26 @@ print("Welcome to my script!")
 
 name = input("Type the name here: ")
 
+if name.isalpha() == False:
+    print("This field is not designed for phone numbers!!!")
+    exit()
+
 pn = input("Type the phone number here: ")
+
+if pn.isdigit() == False:
+    print("This field is not designed for names!!!")
+    exit()
 
 results = input("Type the name or phone number here: ")
 
-if results.isdigit() == False:
+if results != pn and results != name:
+    print("It does not equal name or phone number.")
+    exit()
+
+elif results.isalpha() == True :
     print(pn + " is the phone number")
     exit()
 
 elif results.isdigit() == True:
     print(name + " is the name.")
+    exit()
